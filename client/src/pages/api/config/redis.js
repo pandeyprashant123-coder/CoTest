7fe4076cd2ddbcabd7c6077c8c83041eba9987b4
pandeyprashant123-coder <83771967+pandeyprashant123-coder@users.noreply.github.com>
2025-03-03 13:20:58 +0545

    update: redis url change

--- client/src/pages/api/config/redis.js
+++ client/src/pages/api/config/redis.js
@@ -1,7 +1,7 @@
 import Redis from "ioredis";
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
   tls: process.env.NODE_ENV === "production" ? {} : undefined,
 });
 export default redis;
