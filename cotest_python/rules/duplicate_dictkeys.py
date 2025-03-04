ca4cd14551a2511b0fae626958e40aa4e578f847
Yashwan Kafle <99085569+YashwanKafle@users.noreply.github.com>
2025-03-04 18:18:41 +0545

    fix: fix duplicate keys rules (#43)

--- cotest_python/rules/duplicate_dictkeys.py
+++ cotest_python/rules/duplicate_dictkeys.py
@@ -6,21 +6,35 @@ from cotest_python.rules.baserule import BaseRule
 
 
 class DuplicateDictKeysRule(BaseRule):
+
     def visit_Dict(self, node: ast.Dict) -> None:
         seen = defaultdict(list)
-        for key, value in zip(node.keys, node.values):
+
+        def get_key_repr(key):
             if isinstance(key, ast.Constant):
-                metadata = {"lineno": key.lineno, "key": key.value}
-                seen[key.value].append(metadata)
+                return key.value
+            elif isinstance(key, ast.Name):
+                return key.id
+            elif isinstance(key, ast.Attribute):
+                return f"{get_key_repr(key.value)}.{key.attr}"
+            elif isinstance(key, ast.Tuple):
+                return tuple(get_key_repr(elt) for elt in key.elts)
+
+        for key, value in zip(node.keys, node.values):
             if isinstance(value, ast.Dict):
                 self.visit(value)
+
+            key_repr = get_key_repr(key)
+            if key_repr and key:
+                metadata = {"lineno": key.lineno, "key": key_repr}
+                seen[key_repr].append(metadata)
 
         for key, metadata in seen.items():
             if len(metadata) > 1:
                 lines = ", ".join(str(line["lineno"]) for line in metadata)
                 self.errors.append(
                     CodeError(
-                        severity=1,
+                        severity=2,
                         message=f'Key "{key}" has been repeated on lines {lines}',
                         line=metadata[0]["lineno"],
                         endline=metadata[0]["lineno"],
