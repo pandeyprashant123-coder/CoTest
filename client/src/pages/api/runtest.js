1185026d2ba2fdd0eda2ecc50b36c90952b97682
Yashwan Kafle <99085569+YashwanKafle@users.noreply.github.com>
2025-03-04 18:43:42 +0545

    fix: fix typo (#44)

--- client/src/pages/api/runtest.js
+++ client/src/pages/api/runtest.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
     await Promise.all([
       addFilesToQueue(jsFiles, "Javascript"),
-      addFilesToQueue(pythonFiles, "python"),
+      addFilesToQueue(pythonFiles, "Python"),
     ]);
 
     const allFiles = [...jsFiles, ...pythonFiles];
